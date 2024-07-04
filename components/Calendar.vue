7f4c44d60ac5db35ce7f1da7f722bccf1d30b558
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2024-07-04 22:26:03 +0200

    eezy

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -2,8 +2,8 @@
   <div class="month">
     <h1 class="text-white tracking-wide p-3 capitalize mt-20">vita huset bokningskalender</h1>
     <section class="relative">
-        <button class="prev" @click="previousMonth">&#10094;</button>
-        <button class="next" @click="nextMonth">&#10095;</button>
+        <!-- <button class="prev" @click="previousMonth">&#10094;</button>
+        <button class="next" @click="nextMonth">&#10095;</button> -->
         <h4 class="text-[2rem]">
           {{ monthName }}
           <span class="text-[1.2rem]">{{ year }}</span>
@@ -21,10 +21,14 @@
     <li>Su</li>
   </ul>
 
-  <ul class="days">
-      <li v-for="day in days" :key="day">{{ day }}</li>
-      <!-- <Date :year="year" :month="month" :day="day" :userId="userId" /> -->
-  </ul>
+  <section class="relative">
+      <button id="prev"class="absolute left-1 top-[7rem] cursor-pointer text-5xl w-[2.5rem]" @click="previousMonth">&#10094;</button>
+      <button id="next" class="absolute right-1 top-[7rem] cursor-pointer text-5xl w-[2.5rem]" @click="nextMonth">&#10095;</button>
+      <ul class="days w-full flex flex-wrap justify-center">
+          <li v-for="day in days" :key="day">{{ day }}</li>
+          <!-- <Date :year="year" :month="month" :day="day" :userId="userId" /> -->
+      </ul>
+  </section>
 
   <!-- <ul class="days">
     <li>9</li>
@@ -127,40 +131,32 @@ export default {
      1px  1px 0 #000;
 }
 
-.month .prev {
-  float: left;
+#prev {
   border-radius: 28%;
-  cursor: pointer;
   transition: background-color 0.3s ease, transform 0.2s ease;
-  font-size: 3rem;
-  width: 2.5rem;
 }
 
-.month .prev:hover {
+#prev:hover {
   background-color: rgba(255, 255, 255, 0.6);
   transform: scale(1.1);
 }
 
-.month .prev:active {
+#prev:active {
   background-color: rgba(255, 255, 255, 0.8);
   transform: scale(0.95);
 }
 
-.month .next {
-  float: right;
+#next {
   border-radius: 28%;
-  cursor: pointer;
   transition: background-color 0.3s ease, transform 0.2s ease;
-  font-size: 3rem;
-  width: 2.5rem;
 }
 
-.month .next:hover {
+#next:hover {
   background-color: rgba(255, 255, 255, 0.6);
   transform: scale(1.1);
 }
 
-.month .next:active {
+#next:active {
   background-color: rgba(255, 255, 255, 0.8);
   transform: scale(0.95);
 }
@@ -176,9 +172,11 @@ export default {
   display: inline-block;
   width: 13.6%;
   text-align: center;
-  margin-bottom: 5px;
+  margin: 5px;
   font-size:2rem;
+  border: 2px solid white;
 }
+
 
 .days li .active {
   padding: 5px;
